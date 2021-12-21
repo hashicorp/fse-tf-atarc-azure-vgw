0768b3dd526d86bdc8f4a4f6401e506b02da8043
danfedick <daniel.fedick@hashicorp.com>
2021-12-21 12:47:56 -0500

    Add 2 outputs that are necessary for aws VPN

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,13 @@ output "virtual_network_gateway_name" {
   value       = azurerm_virtual_network_gateway.virtual_network_gateway.name
   description = "Azure Virtual Network Gateway Name"
 }
+
+output "virtual_public_ip_1" {
+  value       = azurerm_public_ip.public_ip_1.tunnel_ip_addresses[0]
+  description = "Public IP Address 1"
+}
+
+output "virtual_public_ip_2" {
+  value       = azurerm_public_ip.public_ip_2.tunnel_ip_addresses[0]
+  description = "Public IP Address 2"
+}
