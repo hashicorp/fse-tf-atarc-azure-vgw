18a4a6c3690f45824c92bec6c43958ca5d2b07ca
danfedick <daniel.fedick@hashicorp.com>
2021-12-21 12:49:51 -0500

    Stub at public_ip_1/2

--- outputs.tf
+++ outputs.tf
@@ -4,11 +4,11 @@ output "virtual_network_gateway_name" {
 }
 
 output "virtual_public_ip_1" {
-  value       = azurerm_public_ip.public_ip_1.tunnel_ip_addresses[0]
+  value       = azurerm_public_ip.public_ip_1
   description = "Public IP Address 1"
 }
 
 output "virtual_public_ip_2" {
-  value       = azurerm_public_ip.public_ip_2.tunnel_ip_addresses[0]
+  value       = azurerm_public_ip.public_ip_2
   description = "Public IP Address 2"
 }
